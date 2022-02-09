0e0325cca9edb49bc5cedba20bb8ac9824508af4
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-02-09 04:55:08 +0000

    Wed, Feb 9, 2022, 4:55 AM +00:00

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-app.use(express.static('.'));
-app.listen(3000);
+app.get('/', (req, res) => {
+  console.log('First Entry');
+  res.redirect(`/${455}`)
+})
+
+app.get('/:room', (req, res) => {
+  console.log('Second Entry');
+  res.send(`This is the url: ${req.url}`);
+})
+
+io.on('connection', socket => {
+  console.log('Third Entry');
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(3000)
