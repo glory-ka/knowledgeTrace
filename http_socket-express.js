db1d1949c7695f6db298855b09600785d4569baf
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-02-09 10:25:29 +0000

    html absolute/relative path, socket.io static vs sendFile

--- http_socket-express.js
+++ http_socket-express.js
@@ -3,22 +3,32 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+/**
+ * app.use(express.static('.'));
+ * The code bellow works well if we only want to see the index.html
+ */
+app.get('/index', (req, res)=>{
+  res.sendFile(__dirname + /index.html);
+});
+
+
 app.get('/', (req, res) => {
   console.log('First Entry');
   res.redirect(`/${455}`)
 })
 
 app.get('/:room', (req, res) => {
+
   console.log('Second Entry');
+  
   res.send(`This is the url: ${req.url}`);
 })
 
 io.on('connection', socket => {
-  console.log('Third Entry');
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
 
+  console.log('Third Entry');
+
+  socket.on('join-room', (roomId, userId) => {
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
--- test2.js
+++
@@ -1,2 +0,0 @@
-const socket = require('socket.io');
-const io = socket.connect('https://glory-ka-code50-57584382-44xjqvx4fw4g-3000.githubpreview.dev/');
